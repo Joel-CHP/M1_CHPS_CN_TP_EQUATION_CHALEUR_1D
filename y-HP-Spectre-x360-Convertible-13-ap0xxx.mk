7f1b0b6a300aca54b1d40d912e6193107db5309a
Joel Bougron <joel.bougron@ens.uvsq.fr>
2022-12-14 17:30:18 +0100

    Ajout fichier .mk correspondant à l'ordinateur du Développeur

---
+++ y-HP-Spectre-x360-Convertible-13-ap0xxx.mk
@@ -0,0 +1,12 @@
+#######################################
+# ambre.mk
+# Default options for ambre computer
+#######################################
+CC=gcc
+LIBSLOCAL=-L/usr/lib -llapack -lblas -lm
+INCLUDEBLASLOCAL=-I/usr/include
+OPTCLOCAL=-fPIC -march=native
+
+# PERSO
+# /usr/include/x86_64-linux-gnu/cblas.h
+# /usr/include/x86_64-linux-gnu/openblas-pthread/cblas.h
